cf342f63691538678a2679fae1ece97d312790b8
Malta <131196172+MaltaSH@users.noreply.github.com>
2023-06-16 20:02:07 +0200

    Update gowp.go
    update

--- gowp.go
+++ gowp.go
@@ -13,7 +13,7 @@ func main() {
 	flag.Parse()
 
 	if *siteURL == "" {
-		fmt.Println("Please specify the website ( https://example.com ) using the -u argument")
+		fmt.Println("Please specify the website (https://example.com) using the -u argument")
 		os.Exit(1)
 	}
 	fmt.Println(" ██████╗  ██████╗ ██╗    ██╗██████╗ ")
@@ -25,8 +25,8 @@ func main() {
 	fmt.Println("")
 	fmt.Println("GOWP v1 - A lightweight WordPress site scanner.")
 	fmt.Println("By MALTA! | I am not responsible for your actions, this script was for educational purposes only.")
-	fmt.Println("BTC : bc1qa454njt7dd8l505puwudwe2wrvcztxd5d44azg | XMR : 46xP9bvZnoR2AUGd6yrpq9RC3XuLJm5KrGT8GtCUrhCtcBJ5M1nnD1gWAnnD6tLtWqKeoJi8UuNsk41K8kE8UYkdTaosjGh")
-	fmt.Println("Session ID : 05f643faf9020221785ecebbef7df21b4c10491e3c07795e264a04132f204c161f")
+	fmt.Println("BTC: bc1qa454njt7dd8l505puwudwe2wrvcztxd5d44azg | XMR: 46xP9bvZnoR2AUGd6yrpq9RC3XuLJm5KrGT8GtCUrhCtcBJ5M1nnD1gWAnnD6tLtWqKeoJi8UuNsk41K8kE8UYkdTaosjGh")
+	fmt.Println("Session ID: 05f643faf9020221785ecebbef7df21b4c10491e3c07795e264a04132f204c161f")
 	fmt.Println("")
 	time.Sleep(2 * time.Second)
 	fmt.Println("[INFO] The scan is in progress... Please wait.")
@@ -34,7 +34,7 @@ func main() {
 	pingWebsite(*siteURL)
 	testWpAdmin(*siteURL)
 	testUpload(*siteURL)
-	fmt.Println("[INFO] Scan for sitesmap in progress...")
+	fmt.Println("[INFO] Scan for sitemaps in progress...")
 	fmt.Println("")
 	testSitemap(*siteURL)
 	fmt.Println("[INFO] Scan for interesting files in progress...")
@@ -46,14 +46,14 @@ func main() {
 func pingWebsite(url string) {
 	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Println("[-] The website is not accessible, scan cancelled.")
+		fmt.Println("[-] The website is not accessible, scan canceled.")
 		os.Exit(1)
 	}
 
 	defer resp.Body.Close()
 
 	fmt.Println("[+] The website is online")
-	fmt.Println("-> Find with a ping")
+	fmt.Println("-> Found with a ping")
 	fmt.Println("")
 }
 
@@ -61,17 +61,18 @@ func testWpAdmin(url string) {
 	adminURL := url + "/wp-admin"
 	resp, err := http.Get(adminURL)
 	if err != nil {
-		fmt.Println("Erreur lors de l'accès à wp-admin :", err)
+		fmt.Println("Error accessing wp-admin")
 		fmt.Println("")
 		return
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode == http.StatusNotFound || resp.StatusCode == http.StatusForbidden {
-		fmt.Println("[-] wp-admin non accessible :", resp.Status)
+		fmt.Println("[-] wp-admin not accessible")
 		fmt.Println("")
 	} else {
 		fmt.Println("[+] wp-admin accessible")
+		fmt.Println("-> Location: " + url + "wp-admin")
 	}
 }
 
@@ -79,7 +80,7 @@ func testUpload(url string) {
 	uploadURL := url + "/wp-content/uploads/"
 	resp, err := http.Get(uploadURL)
 	if err != nil {
-		fmt.Println("[-] Répertoire d'upload non disponible :", err)
+		fmt.Println("[-] Upload directory not available")
 		fmt.Println("")
 		return
 	}
@@ -87,11 +88,12 @@ func testUpload(url string) {
 	defer resp.Body.Close()
 
 	if resp.StatusCode == http.StatusNotFound || resp.StatusCode == http.StatusForbidden {
-		fmt.Println("[-] Répertoire d'upload non accessible :", resp.Status)
+		fmt.Println("[-] Upload directory not accessible")
 		fmt.Println("")
 	} else {
-		fmt.Println("[+] Répertoire d'upload trouvé")
-		fmt.Println("-> Trouvé grâce à une requête directe")
+		fmt.Println("[+] Upload directory found")
+		fmt.Println("-> Found through a direct request")
+		fmt.Println("-> Location: " + url + "wp-content/uploads/")
 		fmt.Println("")
 	}
 }
@@ -108,17 +110,17 @@ func testSitemap(url string) {
 		fullURL := url + path
 		resp, err := http.Get(fullURL)
 		if err != nil {
-			fmt.Printf("[-] %s non accessible: %s\n\n", path, err)
+			fmt.Printf("[-] %s not accessible: %s\n\n", path, err)
 			continue
 		}
 
 		defer resp.Body.Close()
 
 		if resp.StatusCode == http.StatusNotFound || resp.StatusCode == http.StatusForbidden {
-			fmt.Printf("[-] %s non accessible: %s\n\n", path, resp.Status)
+			fmt.Printf("[-] %s not accessible: %s\n\n", path, resp.Status)
 		} else {
-			fmt.Printf("[+] %s trouvé\n", path)
-			fmt.Println("-> Trouvé grâce à une requête directe")
+			fmt.Printf("[+] %s found\n", path)
+			fmt.Println("-> Found through a direct request")
 			fmt.Println("")
 		}
 	}
@@ -128,7 +130,7 @@ func testRobotxt(url string) {
 	robotURL := url + "/robots.txt"
 	resp, err := http.Get(robotURL)
 	if err != nil {
-		fmt.Println("[-] robots.txt non accessible :", err)
+		fmt.Println("[-] robots.txt not accessible")
 		fmt.Println("")
 		return
 	}
@@ -136,11 +138,12 @@ func testRobotxt(url string) {
 	defer resp.Body.Close()
 
 	if resp.StatusCode == http.StatusNotFound || resp.StatusCode == http.StatusForbidden {
-		fmt.Println("[-] robots.txt non accessible :", resp.Status)
+		fmt.Println("[-] robots.txt not accessible")
 		fmt.Println("")
 	} else {
-		fmt.Println("[+] robots.txt trouvé")
-		fmt.Println("-> Trouvé grâce à une requête directe")
+		fmt.Println("[+] robots.txt found")
+		fmt.Println("-> Found through a direct request")
+		fmt.Println("-> Location: " + url + "robots.txt")
 		fmt.Println("")
 	}
 }
@@ -149,21 +152,21 @@ func testXMLRPC(url string) {
 	xmlrpcURL := url + "/xmlrpc.php"
 	resp, err := http.Get(xmlrpcURL)
 	if err != nil {
-		fmt.Println("[-] XMLRPC non accessible :", err)
+		fmt.Println("[-] XMLRPC not accessible:", err)
 		return
 	}
 
 	defer resp.Body.Close()
 
 	if resp.StatusCode == http.StatusNotFound || resp.StatusCode == http.StatusForbidden {
-		fmt.Println("[-] XMLRPC non accessible :", resp.Status)
+		fmt.Println("[-] XMLRPC not accessible:", resp.Status)
 		fmt.Println("")
 	} else {
-		fmt.Println("[+] XMLRPC trouvé")
-		fmt.Println("-> Trouvé grâce à une requête directe")
-		fmt.Println("-> Emplacement :" + url + "/xmlrpc.php")
-		fmt.Println("-> Référence : https://codex.wordpress.org/XML-RPC_WordPress_API")
-		fmt.Println("-> Exploit possible : https://nitesculucian.github.io/2019/07/01/exploiting-the-xmlrpc-php-on-all-wordpress-versions/")
+		fmt.Println("[+] XMLRPC found")
+		fmt.Println("-> Found through a direct request")
+		fmt.Println("-> Location: " + url + "xmlrpc.php")
+		fmt.Println("-> Reference: https://codex.wordpress.org/XML-RPC_WordPress_API")
+		fmt.Println("-> Possible exploit: https://nitesculucian.github.io/2019/07/01/exploiting-the-xmlrpc-php-on-all-wordpress-versions/")
 		fmt.Println("")
 	}
 }
