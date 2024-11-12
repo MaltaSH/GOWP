d00098b7ba0f25e6815d9320b0f02f5e58221496
Malta <131196172+MaltaSH@users.noreply.github.com>
2024-11-12 21:23:00 +0100

    Update gowp.go
    + colors
    + plugins reco
    + theme reco

--- gowp.go
+++ gowp.go
@@ -1,5 +1,7 @@
 package main
 
+// BY MALTASH ON GITHUB
+
 import (
 	"flag"
 	"fmt"
@@ -7,6 +9,16 @@ import (
 	"os"
 	"time"
 )
+
+var Reset = "\033[0m"
+var Red = "\033[31m"
+var Green = "\033[32m"
+var Yellow = "\033[33m"
+var Blue = "\033[34m"
+var Magenta = "\033[35m"
+var Cyan = "\033[36m"
+var Gray = "\033[37m"
+var White = "\033[97m"
 
 func main() {
 	siteURL := flag.String("u", "", "Website URL")
@@ -24,20 +36,21 @@ func main() {
 	fmt.Println(" ╚═════╝  ╚═════╝  ╚══╝╚══╝ ╚═╝     ")
 	fmt.Println("")
 	fmt.Println("GOWP v1 - A lightweight WordPress site scanner.")
-	fmt.Println("By MALTA! | I am not responsible for your actions, this script was for educational purposes only.")
-	fmt.Println("BTC: bc1qa454njt7dd8l505puwudwe2wrvcztxd5d44azg | XMR: 46xP9bvZnoR2AUGd6yrpq9RC3XuLJm5KrGT8GtCUrhCtcBJ5M1nnD1gWAnnD6tLtWqKeoJi8UuNsk41K8kE8UYkdTaosjGh")
-	fmt.Println("Session ID: 05f643faf9020221785ecebbef7df21b4c10491e3c07795e264a04132f204c161f")
+	fmt.Print(Blue + "By MALTASH | " + Reset)
+	fmt.Println(Red + " I am not responsible for your actions, this script was for educational purposes only." + Reset)
 	fmt.Println("")
 	time.Sleep(2 * time.Second)
-	fmt.Println("[INFO] The scan is in progress... Please wait.")
+	fmt.Println(Yellow + "[INFO] The scan is in progress... Please wait." + Reset)
 	fmt.Println("")
 	pingWebsite(*siteURL)
 	testWpAdmin(*siteURL)
 	testUpload(*siteURL)
-	fmt.Println("[INFO] Scan for sitemaps in progress...")
+	testThemeLoc(*siteURL)
+	testPlugin(*siteURL)
+	fmt.Println(Yellow + "[INFO] Scan for sitemaps in progress..." + Reset)
 	fmt.Println("")
 	testSitemap(*siteURL)
-	fmt.Println("[INFO] Scan for interesting files in progress...")
+	fmt.Println(Yellow + "[INFO] Scan for interesting files in progress..." + Reset)
 	fmt.Println("")
 	testRobotxt(*siteURL)
 	testXMLRPC(*siteURL)
@@ -46,13 +59,13 @@ func main() {
 func pingWebsite(url string) {
 	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Println("[-] The website is not accessible, scan canceled.")
+		fmt.Println(Red + "[-] The website is not accessible, scan canceled." + Reset)
 		os.Exit(1)
 	}
 
 	defer resp.Body.Close()
 
-	fmt.Println("[+] The website is online")
+	fmt.Println(Green + "[+] The website is online" + Reset)
 	fmt.Println("-> Found with a ping")
 	fmt.Println("")
 }
@@ -61,18 +74,38 @@ func testWpAdmin(url string) {
 	adminURL := url + "/wp-admin"
 	resp, err := http.Get(adminURL)
 	if err != nil {
-		fmt.Println("Error accessing wp-admin")
+		fmt.Println(Red + "Error accessing wp-admin" + Reset)
 		fmt.Println("")
 		return
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode == http.StatusNotFound || resp.StatusCode == http.StatusForbidden {
-		fmt.Println("[-] wp-admin not accessible")
+		fmt.Println(Red + "[-] wp-admin not accessible" + Reset)
 		fmt.Println("")
 	} else {
-		fmt.Println("[+] wp-admin accessible")
+		fmt.Println(Green + "[+] wp-admin accessible" + Reset)
 		fmt.Println("-> Location: " + url + "wp-admin")
+	}
+}
+
+func testPlugin(url string) {
+	pluginURL := url + "/wp-content/plugins/"
+	resp, err := http.Get(pluginURL)
+	if err != nil {
+		fmt.Println(Red + "[-] Plugins directory not available" + Reset)
+		fmt.Println("")
+		return
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode == http.StatusNotFound || resp.StatusCode == http.StatusForbidden {
+		fmt.Println(Red + "[-] Plugins directory not accessible" + Reset)
+		fmt.Println("")
+	} else {
+		fmt.Println(Green + "[+] Plugins directory found" + Reset)
+		fmt.Println("-> Found through a direct request")
+		fmt.Println("-> Location: " + url + "wp-content/plugins/")
+		fmt.Println("")
 	}
 }
 
@@ -80,7 +113,7 @@ func testUpload(url string) {
 	uploadURL := url + "/wp-content/uploads/"
 	resp, err := http.Get(uploadURL)
 	if err != nil {
-		fmt.Println("[-] Upload directory not available")
+		fmt.Println(Red + "[-] Upload directory not available" + Reset)
 		fmt.Println("")
 		return
 	}
@@ -88,10 +121,10 @@ func testUpload(url string) {
 	defer resp.Body.Close()
 
 	if resp.StatusCode == http.StatusNotFound || resp.StatusCode == http.StatusForbidden {
-		fmt.Println("[-] Upload directory not accessible")
+		fmt.Println(Red + "[-] Upload directory not accessible" + Reset)
 		fmt.Println("")
 	} else {
-		fmt.Println("[+] Upload directory found")
+		fmt.Println(Green + "[+] Upload directory found" + Reset)
 		fmt.Println("-> Found through a direct request")
 		fmt.Println("-> Location: " + url + "wp-content/uploads/")
 		fmt.Println("")
@@ -110,16 +143,16 @@ func testSitemap(url string) {
 		fullURL := url + path
 		resp, err := http.Get(fullURL)
 		if err != nil {
-			fmt.Printf("[-] %s not accessible: %s\n\n", path, err)
+			fmt.Printf(Red+"[-] %s not accessible: %s\n\n"+Reset, path, err)
 			continue
 		}
 
 		defer resp.Body.Close()
 
 		if resp.StatusCode == http.StatusNotFound || resp.StatusCode == http.StatusForbidden {
-			fmt.Printf("[-] %s not accessible: %s\n\n", path, resp.Status)
+			fmt.Printf(Red+"[-] %s not accessible: %s\n\n"+Reset, path, resp.Status)
 		} else {
-			fmt.Printf("[+] %s found\n", path)
+			fmt.Printf(Green+"[+] %s found\n"+Reset, path)
 			fmt.Println("-> Found through a direct request")
 			fmt.Println("")
 		}
@@ -130,7 +163,7 @@ func testRobotxt(url string) {
 	robotURL := url + "/robots.txt"
 	resp, err := http.Get(robotURL)
 	if err != nil {
-		fmt.Println("[-] robots.txt not accessible")
+		fmt.Println(Red + "[-] robots.txt not accessible" + Reset)
 		fmt.Println("")
 		return
 	}
@@ -138,10 +171,10 @@ func testRobotxt(url string) {
 	defer resp.Body.Close()
 
 	if resp.StatusCode == http.StatusNotFound || resp.StatusCode == http.StatusForbidden {
-		fmt.Println("[-] robots.txt not accessible")
+		fmt.Println(Red + "[-] robots.txt not accessible" + Reset)
 		fmt.Println("")
 	} else {
-		fmt.Println("[+] robots.txt found")
+		fmt.Println(Green + "[+] robots.txt found" + Reset)
 		fmt.Println("-> Found through a direct request")
 		fmt.Println("-> Location: " + url + "robots.txt")
 		fmt.Println("")
@@ -152,21 +185,41 @@ func testXMLRPC(url string) {
 	xmlrpcURL := url + "/xmlrpc.php"
 	resp, err := http.Get(xmlrpcURL)
 	if err != nil {
-		fmt.Println("[-] XMLRPC not accessible:", err)
+		fmt.Println(Red+"[-] XMLRPC not accessible:"+Reset, err)
 		return
 	}
 
 	defer resp.Body.Close()
 
 	if resp.StatusCode == http.StatusNotFound || resp.StatusCode == http.StatusForbidden {
-		fmt.Println("[-] XMLRPC not accessible:", resp.Status)
+		fmt.Println(Red+"[-] XMLRPC not accessible:"+Reset, resp.Status)
 		fmt.Println("")
 	} else {
-		fmt.Println("[+] XMLRPC found")
+		fmt.Println(Green + "[+] XMLRPC found" + Reset)
 		fmt.Println("-> Found through a direct request")
 		fmt.Println("-> Location: " + url + "xmlrpc.php")
 		fmt.Println("-> Reference: https://codex.wordpress.org/XML-RPC_WordPress_API")
-		fmt.Println("-> Possible exploit: https://nitesculucian.github.io/2019/07/01/exploiting-the-xmlrpc-php-on-all-wordpress-versions/")
+		fmt.Println(Cyan + "-> Possible exploit: https://nitesculucian.github.io/2019/07/01/exploiting-the-xmlrpc-php-on-all-wordpress-versions/" + Reset)
+		fmt.Println("")
+	}
+}
+
+func testThemeLoc(url string) {
+	themeURL := url + "/wp-content/themes/"
+	resp, err := http.Get(themeURL)
+	if err != nil {
+		fmt.Println(Red+"[-] Themes not accessible:"+Reset, err)
+		return
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode == http.StatusNotFound || resp.StatusCode == http.StatusForbidden {
+		fmt.Println(Red + "[-] Themes not accessible:" + Reset)
+		fmt.Println("")
+	} else {
+		fmt.Println(Green + "[+] Themes dir found" + Reset)
+		fmt.Println("-> Found through a direct request")
+		fmt.Println("-> Location: " + url + "/wp-content/themes/")
+		fmt.Println("-> Reference: https://codex.wordpress.org/Theme_Development")
 		fmt.Println("")
 	}
 }
